46c49a7d8d07c5eac2ea97dd98ce311cd60cc68e
Moiz Asif <moizasif07@gmail.com>
2025-08-14 17:55:42 +0500

    number

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
 
       {/* Floating WhatsApp Button */}
       <a
-        href="https://wa.me/923150007800"
+        href="https://wa.me/923104115955"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white px-5 py-3 rounded-full shadow-lg flex items-center gap-2 transition"
